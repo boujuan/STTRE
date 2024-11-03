4497c33556b5825394f325efee13f6294539b56e
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2024-11-03 21:49:19 +0100

    run

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ export PYTHONUNBUFFERED=1
 
 # Set the number of threads per process (8 CPU cores per GPU (32/4=8))
 export OMP_NUM_THREADS=8
-export NCCL_DEBUG=WARN # Enable NCCL debugging
+# Enable NCCL debugging, choose INFO for more details
+export NCCL_DEBUG=WARN 
 
 # Run with 4 GPUs
 torchrun --nproc_per_node=4 --master_port=29500 ~/STTRE/STTRE_PL.py
